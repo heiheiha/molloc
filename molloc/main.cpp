e456354361f81bca5266962ab38c048e4cf1c15c
heiheiha <1137029429@qq.com>
2015-11-11 21:36:04 +0800

    动态存储分配
    定义一个指针变量，动态分配一个4字节的存储空间，然后输出结果

--- molloc/main.cpp
+++ molloc/main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  molloc
+//  动态分配存储空间
 //
 //  Created by  黄磊 on 15/11/11.
 //  Copyright © 2015年  黄磊. All rights reserved.
@@ -10,6 +10,9 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int *a;
+    a=(int *)malloc(4);
+    *a=100;
+    printf("a=%x  *a=%x\n",a,*a);
     return 0;
 }
